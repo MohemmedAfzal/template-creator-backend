f02c42ae2c572a3b138e2a78fb6e9d2ce1c78e49
mohammed.afzalva <mohammed.afzal@edifecs.com>
2026-01-21 17:46:56 +0530

    feat(AS-Integration): Integrated with Accounts Service

--- src/main/java/com/example/textexpander/config/SecurityConfig.java
+++ src/main/java/com/example/textexpander/config/SecurityConfig.java
@@ -1,25 +1,16 @@
 package com.example.textexpander.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import static com.collablynk.accounts.security.config.ClServiceConfigurer.clService;
 
 @Configuration
 public class SecurityConfig {
-
-    @Autowired
-    private JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -34,39 +25,14 @@ public class SecurityConfig {
                         // Allow signup/login API calls
                         .requestMatchers("/api/auth/**").permitAll()
                         // Protect all other APIs
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                                .requestMatchers("/api/user/**").hasRole("USER")
+                        .requestMatchers("/api/admin/**").hasRole("ADMINISTRATOR")
+                                .requestMatchers("/api/user/**").hasRole("REGULAR_USER")
                         .requestMatchers("/api/**").authenticated()
-//                        .requestMatchers(new AntPathRequestMatcher("/admin.html")).hasRole("ADMIN")
-//                        .requestMatchers(new AntPathRequestMatcher("/user.html")).hasRole("USER")
                         .anyRequest().authenticated()
                 )
                 // No form login or basic auth
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable);
-
-//        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
-    }
-
-    //    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("*"));
-//        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
-//        configuration.setAllowedHeaders(List.of("*"));
-//        configuration.setAllowCredentials(true);
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
